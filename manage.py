d21dd8421382ecb39b6776e47ad50bac2ba5282c
Benjamin Baka <baka@mpedigree.net>
2015-09-24 17:27:29 +0000

    Ben: A few commits to get things started

---
+++ manage.py
@@ -0,0 +1,11 @@
+from flask_script import Manager
+#from flask.ext.script import Manager
+
+from sched.app import app
+
+manager = Manager(app)
+
+app.config['DEBUG'] = True
+
+if __name__ == "__main__":
+    manager.run()
--- sched/app.py
+++ sched/app.py
@@ -1,10 +1,19 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, world !"
+    return render_template("index.html", my_age=28)
+
+@app.context_processor
+def luck_processor():
+    from random import randint
+    def lucky_number():
+        return randint(1, 10)
+
+    return dict(lucky_number=lucky_number)
+
+
 
 
 if __name__ == "__main__":
