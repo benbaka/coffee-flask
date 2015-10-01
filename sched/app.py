8725216bd13d3baa70f51a0943233da928bfc1d3
Benjamin Baka <baka@mpedigree.net>
2015-10-01 19:15:11 +0000

    Ben: use of the request global variable

--- sched/app.py
+++ sched/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask import request
 app = Flask(__name__)
 
 @app.route("/")
@@ -17,6 +18,10 @@ def luck_processor():
 def user(name):
     return "<h1>Hello, %s!</h1>" % name
 
+@app.route('/index')
+def index():
+    user_agent = request.headers.get("User-Agent")
+    return "<p>Your browser is %s</p>" % user_agent
 
 if __name__ == "__main__":
     app.run()
